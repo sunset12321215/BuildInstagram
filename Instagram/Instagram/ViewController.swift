c6138a116fcc5ffae6bee3085b91ac7c62e897c1
Sun <vu.xuan.cuong@framgia.com>
2019-03-23 17:15:08 +0700

    [Task #01] Initial Project

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by mac on 3/23/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
